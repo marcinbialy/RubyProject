29fde8d33efeed79a6caca71d690d9c4cb83aa23
Marcin Białkowski <bialkowski.marcin91@gmail.com>
2019-02-11 20:07:16 +0100

    Adding new and create actions for portfsController

--- app/controllers/portfs_controller.rb
+++ app/controllers/portfs_controller.rb
@@ -1,5 +1,25 @@
 class PortfsController < ApplicationController
+	 before_action :set_portf, only: [:show, :edit, :update, :destroy]
 	def index
 		@portfolio_items = Portf.all
 	end
+
+	def new
+		@portfolio_item = Portf.new
+	end
+
+	def create
+    @portfolio_item = Portf.new(params.require(:portf).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfs_path, notice: 'Blog was successfully created.' }
+        format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
