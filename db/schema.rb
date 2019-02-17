0618bc2bcca39f09ef74e1de8ec75e6c3f1cbcab
Marcin Białkowski <bialkowski.marcin91@gmail.com>
2019-02-17 20:34:25 +0100

    Adding status property and enum to BlogModel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_172028) do
+ActiveRecord::Schema.define(version: 2019_02_17_191712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_02_13_172028) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
