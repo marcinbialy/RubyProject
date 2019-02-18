23280d4fb3f958777d74de7b6d1da09874c0cb9a
Marcin Białkowski <bialkowski.marcin91@gmail.com>
2019-02-18 19:03:52 +0100

    Implement form configuration for nested attributes portfolio new view

--- app/controllers/portfs_controller.rb
+++ app/controllers/portfs_controller.rb
@@ -10,10 +10,11 @@ class PortfsController < ApplicationController
 
 	def new
 		@portfolio_item = Portf.new
+    3.times {@portfolio_item.technologies.build}
 	end
 
 	def create
-    @portfolio_item = Portf.new(params.require(:portf).permit(:title, :subtitle, :body))
+    @portfolio_item = Portf.new(params.require(:portf).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
