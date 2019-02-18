af3b84e47980bc9e1b4b593e31eccd4c5e26d035
Marcin Białkowski <bialkowski.marcin91@gmail.com>
2019-02-18 15:54:19 +0100

    Adding custom scopes for portfolo model

--- app/models/portf.rb
+++ app/models/portf.rb
@@ -1,4 +1,10 @@
 class Portf < ApplicationRecord
 
 	validates_presence_of :title, :body, :main_image, :thumb_image
+
+	def self.dotnet
+		where(subtitle: '.NET')
+	end
+
+	scope :ruby_portfolio_items, -> {where(subtitle: 'Ruby on Rails')}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+3.times do |topic|
+Topic.create!(
+	title: "Topic #{topic}"
+	)
+end
+
 10.times do |blog|
 Blog.create!(
 	title: "Blog Post nr #{blog}",
-	body: "lorejn dindi ijdnidn sindn dniondixnd dj idniuxndiid xdondnxidnd ixdjnindx d ijn iun un in onn iindd dj dn djnd"
+	body: "lorejn dindi ijdnidn sindn dniondixnd dj idniuxndiid xdondnxidnd ixdjnindx d ijn iun un in onn iindd dj dn djnd",
+	topic_id: Topic.last.id
 	)
 end
 
@@ -16,10 +23,20 @@ end
 
 puts "5 posts created"
 
-9.times do |portfolio|
+8.times do |portfolio|
 	Portf.create!(
 		title: "Portfolio title: #{portfolio}",
-		subtitle: "Services",
+		subtitle: "Ruby on Rails",
+		body: "jno nkjon ioj o klkmklmdondj d ijnnnsk djindjndn dkj dkj dj dkjh dkjh kjhd kjhd h djhd jd jhd jhd jhd djh djh dhj djhd hd jhd djh dhinoiinkj non jnon onfkj dijnnej hijfjhf  kdjd jh",
+		main_image: "http://placehold.it/600x400",
+		thumb_image:"http://placehold.it/350x200"
+		)
+end
+
+1.times do |portfolio|
+	Portf.create!(
+		title: "Portfolio title: #{portfolio}",
+		subtitle: ".NET",
 		body: "jno nkjon ioj o klkmklmdondj d ijnnnsk djindjndn dkj dkj dj dkjh dkjh kjhd kjhd h djhd jd jhd jhd jhd djh djh dhj djhd hd jhd djh dhinoiinkj non jnon onfkj dijnnej hijfjhf  kdjd jh",
 		main_image: "http://placehold.it/600x400",
 		thumb_image:"http://placehold.it/350x200"
