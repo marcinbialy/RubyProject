86d456782fb867aaf28033d0ee2f7549ff3c2d09
Marcin Białkowski <bialkowski.marcin91@gmail.com>
2019-02-19 13:31:56 +0100

    Customize login, logout and registration routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :portfs, except: [:show, :index]
   get 'portfolios', to: 'portfs#index', as: 'portfolio_index'
   get 'portfolio/:id', to: 'portfs#show', as: 'portfolio_show'
