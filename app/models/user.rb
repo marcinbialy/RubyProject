91de099c9ccb624d17ad3e9d8213bc585b9b64e4
Marcin Białkowski <bialkowski.marcin91@gmail.com>
2019-02-19 15:46:28 +0100

    Adding authentication with virtual attributes for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  validates_presence_of :name
+
+  def first_name
+  	self.name.split.first
+  end
+
+  def last_name
+  	if self.name.split.count > 2
+  		last = self.name.split.slice(1 .. -1).join(" ")
+  	else
+  		self.name.split.last
+  	end
+  end
+         
 end
