66cbbd5dc11885f531ff7485aa344d6066a9f3f5
Marcin Białkowski <bialkowski.marcin91@gmail.com>
2019-02-25 22:01:14 +0100

    Refactoring alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -61,6 +61,18 @@ module ApplicationHelper
       "active" if current_page? path
     end
 
+    # creating a notification helper
+    def alerts
+      alert = (flash[:notice] || flash[:error] || flash[:alert])
+        if alert
+          alert_generator alert
+        end
+    end
+
+    def alert_generator msg
+        js add_gritter(msg, title: "MB Project", sticky: false)
+    end
+
 end
 
 module MBViewTool
