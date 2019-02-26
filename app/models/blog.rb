6323fd7d27aee92c972c3d4a60db56faf44eb487
Marcin Białkowski <bialkowski.marcin91@gmail.com>
2019-02-26 20:13:14 +0100

    Adding redis library and creating comment generator

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,6 +3,8 @@ class Blog < ApplicationRecord
 	extend FriendlyId
   	friendly_id :title, use: :slugged
 
+  	has_many :comments, dependent: :destroy
+
   	validates_presence_of :title, :body
 
   	belongs_to :topic, optional: true
