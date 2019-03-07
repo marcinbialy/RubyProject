565ac3da124d6530113bfc64eb9cac1fc28411a5
Marcin Białkowski <bialkowski.marcin91@gmail.com>
2019-03-07 14:38:41 +0100

    Adding topic drop down list for blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,7 +5,7 @@ class Blog < ApplicationRecord
 
   	has_many :comments, dependent: :destroy
 
-  	validates_presence_of :title, :body
+  	validates_presence_of :title, :body, :topic_id
 
   	belongs_to :topic, optional: true
 
