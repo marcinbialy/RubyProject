3d9df5427a785fedab46d3ce74e44a58b69dff97
Marcin Białkowski <bialkowski.marcin91@gmail.com>
2019-03-07 15:24:21 +0100

    Adding topic views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :topics, only: [:show, :index] 
+
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :portfs, except: [:show, :index] 
 
